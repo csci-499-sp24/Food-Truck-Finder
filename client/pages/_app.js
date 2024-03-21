8690cba1ebe762c4c0e95ca0c74d15bb1bee6fb7
Mouhameddia96 <73255427+Mouhameddia96@users.noreply.github.com>
2024-03-21 18:56:27 -0400

    Front Page

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,10 @@
 import "@/styles/globals.css";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <APIProvider apiKey={"AIzaSyB5rbeg6IKKUtIwjfpEO-ydnqXnwosQJ6Q"}>
+      <Component {...pageProps} />
+    </APIProvider>
+  );
 }
