0b84ef875251383d18276c69d2e9d9c8ba24ef91
Alan Hu <64699846+Ailun-Hu@users.noreply.github.com>
2024-03-23 16:43:05 -0400

    Added api provider for map

--- client/compenents/ftMap.js
+++ client/compenents/ftMap.js
@@ -1,21 +1,23 @@
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
+
 export default function FTMap(props) {
     return (
         <>
-          <Map
-            center={props.position}
-            zoom={15}
-            style={{
-              height: 400,
-              minWidth: 400,
-              width: "100%",
-              borderRadius: 16,
-            }}
-            
-          >
-            <Marker position={props.position} />
-          </Map>
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_API_KEY}>
+            <Map
+              center={props.position}
+              zoom={15}
+              style={{
+                height: 300,
+                minWidth: 300,
+                width: "100%",
+                borderRadius: 16,
+              }}
+            >
+              <Marker position={props.position} />
+            </Map>
+          </APIProvider>
         </>
     )
 }
