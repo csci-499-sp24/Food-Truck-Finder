8608cede2d81aacf511d2c2b2afc7060db90c58b
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2024-04-03 21:03:09 -0400

    Implemented the review page functionality

--- client/pages/foodtruck/[id]/addReview.js
+++ client/pages/foodtruck/[id]/addReview.js
@@ -4,6 +4,7 @@ import styles from "../truck.module.css";
 import {
     Stack, Button, Typography, Card, TextField, Rating
 } from "@mui/material";
+import { getSession } from '@/compenents/lib';
 
 export default function AddReview() {
     const router = useRouter();
@@ -11,6 +12,8 @@ export default function AddReview() {
     const [foodTruck, setFoodTruck] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [rating, setRating] = useState(null);
+    const [review, setReview] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -57,8 +60,35 @@ export default function AddReview() {
         );
     }
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const reviewData = {
+                Rating: e.target.rating.value,
+                Review: review,
+                Session: getSession()
+            }
+
+            const response = await fetch(`/api/foodtrucks/${id}/addReview`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(reviewData)
+            });
+
+            console.log('Added a review successfully');
+
+            // Redirect back to foodtruck page
+            router.back();
+        } catch (error) {
+            console.error("Error adding a review:", error);
+        }
+    }
+
     return (
-        <section className={styles.container}>
+        <section className={styles.container} style={{ height: "100vh" }}>
             <Stack direction="column" gap={4} padding={2}>
                 <Typography
                     color={"white"}
@@ -70,7 +100,7 @@ export default function AddReview() {
                 </Typography>
             </Stack>
 
-            <Stack direction="column" justifyContent="center" alignItems="center" paddingTop={4} paddingBottom={14} gap={8}>
+            <Stack direction="column" justifyContent="center" alignItems="center" paddingTop={4} gap={8}>
                 <Card sx={{ backgroundColor: "#105372", padding: 2, width: "50%", overflowY: "auto" }}>
                     <Typography
                         color={"white"}
@@ -82,13 +112,17 @@ export default function AddReview() {
                         {foodTruck.foodTruck.name}
                     </Typography>
 
-                    <form style = {{ margin: "auto", marginBottom: "20px", textAlign: "center" }}>
-                        <div style = {{ padding: "10px 0" }}>
+                    <form onSubmit={handleSubmit} style={{ margin: "auto", marginBottom: "20px", textAlign: "center" }}>
+                        <div style={{ padding: "10px 0" }}>
                             <Typography color="white" variant="h6" align="center">
                                 Overall Rating
                             </Typography>
                             <Rating
                                 name="rating"
+                                value={rating}
+                                onChange={(event, newValue) => {
+                                    setRating(newValue);
+                                }}
                             />
                         </div>
 
@@ -99,14 +133,18 @@ export default function AddReview() {
                             multiline
                             rows={7}
                             variant="outlined"
+                            value={review}
+                            onChange={(e) => setReview(e.target.value)}
                         >
                         </TextField>
+
+                        <div style={{ marginTop: "30px" }}>
+                            <Button variant="outlined" type="submit">
+                                Submit
+                            </Button>
+                        </div>
                     </form>
                 </Card>
-
-                <Button variant="outlined">
-                    Submit
-                </Button>
             </Stack>
         </section>
     );
