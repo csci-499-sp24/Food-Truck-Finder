85e03cbbdfa8ab9cab2023365b0a1ecfd330abb8
Mouhameddia96 <73255427+Mouhameddia96@users.noreply.github.com>
2024-04-04 23:14:02 -0400

    Added a Vegan and Halal filter in the search bar

--- client/compenents/Menu.js
+++ client/compenents/Menu.js
@@ -1,79 +1,100 @@
-import {
-    Button,
-    Card,
-    Stack,
-    Typography,
-  } from "@mui/material";
-  import { useEffect, useState } from "react";
+import { Button, Card, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
+export default function Menu(props) {
+  const foodTruck = props.foodTruck;
 
-export default function Menu(props){
-    const foodTruck = props.foodTruck;
+  // State to track the active view
+  const [activeView, setActiveView] = useState("menu");
 
-    // State to track the active view
-    const [activeView, setActiveView] = useState("menu");
-
-
-    // Event handler for Menu button
-    const handleMenuClick = () => {
+  // Event handler for Menu button
+  const handleMenuClick = () => {
     setActiveView("menu");
-    };
-    // Event handler for Promotions/Events button
-    const handlePromotionsClick = () => {
+  };
+  // Event handler for Promotions/Events button
+  const handlePromotionsClick = () => {
     setActiveView("promotions");
-    };
-    const menuItems = foodTruck.menu && foodTruck.menu.length > 0 ?(
-      foodTruck.menu.map((item, index)=> (
-        <div key={index} style={{ backgroundColor: '#02092c', padding: '10px', margin: '10px', borderRadius: '10px', color: "white" }}>
-          {typeof item.price === 'number' ? 
-            <h4 key={index}>{item.item}: ${item.price.toFixed(2)}</h4> :
-            <h4 key={index}>{item.item}: ${parseFloat(item.price).toFixed(2)}</h4>
-          }
+  };
+  const menuItems =
+    foodTruck.menu && foodTruck.menu.length > 0 ? (
+      foodTruck.menu.map((item, index) => (
+        <div
+          key={index}
+          style={{
+            backgroundColor: "#02092c",
+            padding: "10px",
+            margin: "10px",
+            borderRadius: "10px",
+            color: "white",
+          }}
+        >
+          {typeof item.price === "number" ? (
+            <h4 key={index}>
+              {item.item}: ${item.price.toFixed(2)}
+            </h4>
+          ) : (
+            <h4 key={index}>
+              {item.item}: ${parseFloat(item.price).toFixed(2)}
+            </h4>
+          )}
         </div>
       ))
-    ):(
+    ) : (
       <p>No menu available</p>
     );
-    
-    const promotions = foodTruck.events && foodTruck.events.length > 0 ? (
+
+  const promotions =
+    foodTruck.events && foodTruck.events.length > 0 ? (
       foodTruck.events.map((event, index) => (
-          <div key={index} style={{ backgroundColor: '#02092c', padding: '10px', margin: '10px', borderRadius: '10px', color: "white" }}>
-              <h3>{event.start_date.substring(0, 10) + ' - ' + event.end_date.substring(0, 10)}</h3>
-              <p>{event.event_description}</p>
-          </div>
+        <div
+          key={index}
+          style={{
+            backgroundColor: "#02092c",
+            padding: "10px",
+            margin: "10px",
+            borderRadius: "10px",
+            color: "white",
+          }}
+        >
+          <h3>
+            {event.start_date.substring(0, 10) +
+              " - " +
+              event.end_date.substring(0, 10)}
+          </h3>
+          <p>{event.event_description}</p>
+        </div>
       ))
-    ):(
+    ) : (
       <p>No promotions available</p>
     );
 
-    console.log(foodTruck.events)
-    return (
-        <>
-            <Card sx={{ backgroundColor: "#105372", padding: 3, height: "430px", overflowY: "auto" }}>
-            <Stack direction={"row"} gap={1}>
-              <Button variant="outlined" onClick={handleMenuClick}>
-                Menu
-              </Button>
-              <Button variant="outlined" onClick={handlePromotionsClick}>
-                Promotions/Events
-              </Button>
-            </Stack> 
-            {/* Conditionally rendering content */}
-            {activeView === "menu" && (
-              <div>
-                {menuItems}
-              </div>
-            )}
+  console.log(foodTruck.events);
+  return (
+    <>
+      <Card
+        sx={{
+          backgroundColor: "#105372",
+          padding: 3,
+          height: "430px",
+          overflowY: "auto",
+        }}
+      >
+        <Stack direction={"row"} gap={1}>
+          <Button variant="outlined" onClick={handleMenuClick}>
+            Menu
+          </Button>
+          <Button variant="outlined" onClick={handlePromotionsClick}>
+            Promotions/Events
+          </Button>
+        </Stack>
+        {/* Conditionally rendering content */}
+        {activeView === "menu" && <div>{menuItems}</div>}
 
-            {activeView === "promotions" && (
-              <div>
-                {promotions}
-              </div>
-            )}
+        {activeView === "promotions" && <div>{promotions}</div>}
 
-            <Typography color="white"></Typography>
-            <pre></pre>
-            </Card>
-        </>
-    )
+        <Typography color="white"></Typography>
+        <pre></pre>
+      </Card>
+    </>
+  );
 }
--- client/compenents/sidebar.js
+++ client/compenents/sidebar.js
@@ -1,130 +1,243 @@
 import { useEffect, useState } from "react";
 import { useDebounce } from "use-debounce";
 import Link from "next/link";
-import { getCookie, hasCookie, deleteCookie } from 'cookies-next';
-import {useRouter} from "next/router";
+import { getCookie, hasCookie, deleteCookie } from "cookies-next";
+import { useRouter } from "next/router";
 import { Button } from "@mui/material";
 import { logout } from "./lib";
 
-require('dotenv').config();
+require("dotenv").config();
 
-function Sidebar({ setSelectedTruck, visibleMarkers, setCenter }){
-    const router = useRouter();
-    const [searchFoodTrucks, setSearchFoodTrucks] = useState([]);
-    const [searchTerm, setSearchTerm] = useState("");
-    const languages = ["English", "Spanish"]; // List of languages
+function Sidebar({ setSelectedTruck, visibleMarkers, setCenter }) {
+  const router = useRouter();
+  const [searchFoodTrucks, setSearchFoodTrucks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isVeganChecked, setIsVeganChecked] = useState(false);
+  const [isHalalChecked, setIsHalalChecked] = useState(false);
 
-    const [debounceSearch] = useDebounce(searchTerm, 500);
+  const languages = ["English", "Spanish"]; // List of languages
 
-    useEffect(() => {
-      if(debounceSearch == "") return;
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/searchFoodTrucks?search=${searchTerm}`);
-                const data = await response.json();
-                setSearchFoodTrucks(data.FoodTrucks);
-            } catch (error) {
-                console.error("Error fetching food trucks:", error);
-            }
-        };
-        fetchData();
-    }, [debounceSearch]);
-    const signout = async () => {
-      await logout();
-      router.reload();
-    }
+  const [debounceSearch] = useDebounce(searchTerm, 500);
+  const defaultSearch = "";
 
-    const handleTruckHover = (truck) => {
-      setSelectedTruck(truck);
-      const cords = {lat: truck.lat, lng: truck.lng};
-      setCenter(cords);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const searchQuery = debounceSearch.trim()
+          ? `search=${debounceSearch.trim()}`
+          : "search=";
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/searchFoodTrucks?${searchQuery}`
+        );
+        const data = await response.json();
+        let filteredTrucks = data.FoodTrucks;
+
+        if (isVeganChecked || isHalalChecked) {
+          filteredTrucks = filteredTrucks.filter((truck) => {
+            return (
+              (!isVeganChecked || truck.vegan) &&
+              (!isHalalChecked || truck.halal)
+            );
+          });
+        }
+
+        setSearchFoodTrucks(filteredTrucks);
+      } catch (error) {
+        console.error("Error fetching food trucks:", error);
+      }
     };
-    return (
-        <div className="sidebar" style={{ width: "20%", backgroundColor: "black", padding: "20px" }}>
-          {/* If logged in */}
-          {hasCookie('name') && 
-            <>
-              <a style={{marginRight: "10px", color: "white"}}>{"Hello, " + getCookie("name")}</a>
-              <Button onClick={signout}>Sign-out</Button>
-            </>
-          }
-          {/* Sign-in and sign-up buttons */}
-          {!hasCookie('name') && <div style={{ marginBottom: "20px" }}>
-            <Link legacyBehavior href='/login'>
-              <a className="btn btn-primary" style={{ cursor: "pointer", marginRight: "10px" }}>Sign In</a>
-            </Link>
-            <Link legacyBehavior href='/signup'>
-              <a className="btn btn-secondary" style={{ cursor: "pointer" }}>Sign Up</a>
-            </Link>
-          </div>}
-          {/* Sidebar content goes here */}
-          <h1 style={{ padding: "10px 20px", textAlign: "center", fontWeight: "bold", color: "white", fontSize: "1.5em" }}>Food Truck Finder</h1>
-          {/* Searched Truck */}
-          <div style={{ textAlign: "left", marginBottom: "20px", paddingLeft: "20px" }}>
-            <input
-              type="text"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Search Food Trucks"
-              style={{ width: "100%", marginBottom: "10px", padding: "10px", fontSize: "1em" }}
-            />
-            <ul style={{ color: "white" }}>
-              {searchFoodTrucks.map((foodTruck) => (
-                <li
-                  key={foodTruck.id}
-                  onMouseEnter={(e) => e.stopPropagation()}
-                >
-                  <span onMouseEnter={() => handleTruckHover(foodTruck)}>
+    fetchData();
+  }, [debounceSearch, isVeganChecked, isHalalChecked]);
+
+  const signout = async () => {
+    await logout();
+    router.reload();
+  };
+
+  const handleTruckHover = (truck) => {
+    setSelectedTruck(truck);
+    const cords = { lat: truck.lat, lng: truck.lng };
+    setCenter(cords);
+  };
+  return (
+    <div
+      className="sidebar"
+      style={{ width: "20%", backgroundColor: "black", padding: "20px" }}
+    >
+      {/* If logged in */}
+      {hasCookie("name") && (
+        <>
+          <a style={{ marginRight: "10px", color: "white" }}>
+            {"Hello, " + getCookie("name")}
+          </a>
+          <Button onClick={signout}>Sign-out</Button>
+        </>
+      )}
+      {/* Sign-in and sign-up buttons */}
+      {!hasCookie("name") && (
+        <div style={{ marginBottom: "20px" }}>
+          <Link legacyBehavior href="/login">
+            <a
+              className="btn btn-primary"
+              style={{ cursor: "pointer", marginRight: "10px" }}
+            >
+              Sign In
+            </a>
+          </Link>
+          <Link legacyBehavior href="/signup">
+            <a className="btn btn-secondary" style={{ cursor: "pointer" }}>
+              Sign Up
+            </a>
+          </Link>
+        </div>
+      )}
+      {/* Sidebar content goes here */}
+      <h1
+        style={{
+          padding: "10px 20px",
+          textAlign: "center",
+          fontWeight: "bold",
+          color: "white",
+          fontSize: "1.5em",
+        }}
+      >
+        Food Truck Finder
+      </h1>
+      <div
+        style={{ textAlign: "left", marginBottom: "20px", paddingLeft: "20px" }}
+      >
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search Food Trucks"
+          style={{
+            width: "100%",
+            marginBottom: "10px",
+            padding: "10px",
+            fontSize: "1em",
+          }}
+        />
+        <div>
+          <input
+            type="checkbox"
+            id="vegan"
+            checked={isVeganChecked}
+            onChange={() => setIsVeganChecked(!isVeganChecked)}
+          />
+          <label style={{ color: "white" }} htmlFor="vegan">
+            Vegan
+          </label>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="halal"
+            checked={isHalalChecked}
+            onChange={() => setIsHalalChecked(!isHalalChecked)}
+          />
+          <label style={{ color: "white" }} htmlFor="halal">
+            Halal
+          </label>
+        </div>
+        <ul style={{ color: "white" }}>
+          {searchFoodTrucks.map((foodTruck) => {
+            {
+            }
+
+            return (
+              <li key={foodTruck.id} onMouseEnter={(e) => e.stopPropagation()}>
+                <span onMouseEnter={() => handleTruckHover(foodTruck)}>
                   <Link legacyBehavior href={`/foodtruck/${foodTruck.id}`}>
-                    <a style={{ textDecoration: 'none', color: 'inherit', transition: 'text-decoration 0.3s' }}>
-                      <span 
-                        onMouseEnter={(e) => e.target.style.textDecoration = 'underline'}
-                        onMouseLeave={(e) => e.target.style.textDecoration = 'none'}
+                    <a
+                      style={{
+                        textDecoration: "none",
+                        color: "inherit",
+                        transition: "text-decoration 0.3s",
+                      }}
+                    >
+                      <span
+                        onMouseEnter={(e) =>
+                          (e.target.style.textDecoration = "underline")
+                        }
+                        onMouseLeave={(e) =>
+                          (e.target.style.textDecoration = "none")
+                        }
                       >
                         {foodTruck.name}
                       </span>
                     </a>
                   </Link>
-                  </span>
-                </li>
-              ))}
-            </ul>
-          </div>
+                </span>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
 
-          {/* Trucks near you */}
-          <div style={{ textAlign: "left", marginBottom: "20px", paddingLeft: "20px" }}>
-            <h2 style={{ padding: "10px 20px", textAlign: "center", fontWeight: "bold", color: "white", fontSize: "1.5em" }}>Trucks near you</h2>
-            <ul style={{ color: "white" }}>
-              {visibleMarkers.map((foodTruck) => (
-                <li
-                  key={foodTruck.id}
-                  onMouseEnter={(e) => e.stopPropagation()}
+      {/* Trucks near you */}
+      <div
+        style={{ textAlign: "left", marginBottom: "20px", paddingLeft: "20px" }}
+      >
+        <h2
+          style={{
+            padding: "10px 20px",
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "white",
+            fontSize: "1.5em",
+          }}
+        >
+          Trucks near you
+        </h2>
+        <ul style={{ color: "white" }}>
+          {visibleMarkers.map((foodTruck) => (
+            <li key={foodTruck.id} onMouseEnter={(e) => e.stopPropagation()}>
+              <span onMouseEnter={() => handleTruckHover(foodTruck)}>
+                <Link legacyBehavior href={`/foodtruck/${foodTruck.id}`}>
+                  <a
+                    style={{
+                      textDecoration: "none",
+                      color: "inherit",
+                      transition: "text-decoration 0.3s",
+                    }}
+                  >
+                    <span
+                      onMouseEnter={(e) =>
+                        (e.target.style.textDecoration = "underline")
+                      }
+                      onMouseLeave={(e) =>
+                        (e.target.style.textDecoration = "none")
+                      }
                     >
-                    <span onMouseEnter={() => handleTruckHover(foodTruck)}>
-                    <Link legacyBehavior href={`/foodtruck/${foodTruck.id}`}>
-                      <a style={{ textDecoration: 'none', color: 'inherit', transition: 'text-decoration 0.3s' }}>
-                        <span 
-                          onMouseEnter={(e) => e.target.style.textDecoration = 'underline'}
-                          onMouseLeave={(e) => e.target.style.textDecoration = 'none'}
-                        >
-                          {foodTruck.name}
-                        </span>
-                      </a>
-                    </Link>
+                      {foodTruck.name}
                     </span>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          {/* Language dropdown */}
-          <select style={{ position: "absolute", bottom: "10px", left: "10px", backgroundColor: "black", color: "white", padding: "5px", border: "none" }}>
-            {languages.map((language, index) => (
-              <option key={index}>{language}</option>
+                  </a>
+                </Link>
+              </span>
+            </li>
           ))}
-          </select>
-        </div>
-    );
-  }
-  
+        </ul>
+      </div>
+
+      {/* Language dropdown */}
+      <select
+        style={{
+          position: "absolute",
+          bottom: "10px",
+          left: "10px",
+          backgroundColor: "black",
+          color: "white",
+          padding: "5px",
+          border: "none",
+        }}
+      >
+        {languages.map((language, index) => (
+          <option key={index}>{language}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
 export default Sidebar;
