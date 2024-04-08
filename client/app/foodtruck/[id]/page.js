5c61b0bc4203ac3d244234105c9c31ddb949a345
Alan Hu <64699846+Alan-Hu123@users.noreply.github.com>
2024-04-07 22:10:40 -0400

    update foodtruck page link in server component

--- client/app/foodtruck/[id]/page.js
+++ client/app/foodtruck/[id]/page.js
@@ -1,7 +1,3 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import Image from "next/image";
 import styles from "../truck.module.css";
 import {
   Stack, Button
@@ -12,68 +8,40 @@ import Menu from "../../../compenents/Menu";
 import Address from "../../../compenents/address";
 import FTTitle from '../../../compenents/Title';
 import DropDown from "../../../compenents/dropDown";
+import Link from "next/link";
 require("dotenv").config();
 
 
 
-export default function Page(slug) {
-  const router = useRouter();
+
+const fetchData = async (id) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/foodtrucks/${id}/info`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch food truck");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching food truck:", error);
+  }
+};
+
+export default async function Page(slug) {
   const id = slug.params.id;
 
-  const [foodTruck, setFoodTruck] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const data = await fetchData(id);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_SERVER_URL}/api/foodtrucks/${id}/info`
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch food truck");
-        }
-        const data = await response.json();
-        setFoodTruck(data);
-        setLoading(false);
-        if(foodTruck)
-            console.log(foodTruck.menu);
-      } catch (error) {
-        console.error("Error fetching food truck:", error);
-        setError(error);
-        setLoading(false);
-      }
-    };
-    if (id) {
-      fetchData();
-    }
-  }, [id]);
+  const foodTruck = data;
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return (
-      <div style={{ color: "red", margin: "auto" }}>
-        <div
-          style={{ color: "red", margin: "auto", width: "50%", height: "50%" }}
-          className="errorMessage"
-        >
-          Error: {error.message}
-          <Image src="/404error.webp" height= "1000" width="1000"></Image>
-        </div>
-      </div>
-    );
-  }
   if (!foodTruck) {
     return <div>No food truck found.</div>;
   }
   const position = {
     lat: Number(foodTruck.foodTruck.lat),
     lng: Number(foodTruck.foodTruck.lng),
-  };
-  const navigateToHome = () => {
-    router.push("/");
   };
   return (
     <section className={styles.container}>
@@ -83,8 +51,8 @@ export default function Page(slug) {
             <FTTitle foodTruck={foodTruck}></FTTitle>
             <Address foodTruck ={foodTruck}></Address>
             <Menu foodTruck ={foodTruck}></Menu>
-            <Button variant="outlined" onClick={navigateToHome}>
-              Back to Home
+            <Button variant="outlined" >
+              <Link href={"/"} style={{textDecoration: 'none', color: 'white'}} >Home</Link>
             </Button>
         </Stack>
 
