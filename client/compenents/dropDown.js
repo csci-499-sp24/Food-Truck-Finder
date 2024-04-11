13826d177cc68c63dcd0809a00aebb0d5c25eac8
Alan Hu <64699846+Alan-Hu123@users.noreply.github.com>
2024-04-10 22:29:59 -0400

    Added Buttons for sharing social media sharing

--- client/compenents/dropDown.js
+++ client/compenents/dropDown.js
@@ -1,17 +1,21 @@
+'use client'
 import {
   FormControl,
   InputLabel,
   Select,
   MenuItem,
 } from "@mui/material";
+import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from 'next-share';
 
-export default function DropDown() {
+export default function DropDown(props) {
+  const URL = props.URL;
+  console.log(URL);
   return (
       <>
 
 <FormControl>
       <InputLabel id="demo-simple-select-label">Share Via</InputLabel>
-      <Select
+      {/* <Select
         color="secondary"
         labelId="demo-simple-select-label"
         id="demo-simple-select"
@@ -26,7 +30,15 @@ export default function DropDown() {
         <MenuItem value={10}>Facebook</MenuItem>
         <MenuItem value={20}>INSTAGRAM</MenuItem>
         <MenuItem value={30}>URL</MenuItem>
-      </Select>
+      </Select> */}
+      <FacebookShareButton
+        url = {URL}
+      >
+        <FacebookIcon size={32} />
+      </FacebookShareButton>
+      <TwitterShareButton url={URL}> 
+        <TwitterIcon size={32} />
+      </TwitterShareButton>
     </FormControl>
     <FormControl>
       <InputLabel id="demo-simple-select-label">Language</InputLabel>
