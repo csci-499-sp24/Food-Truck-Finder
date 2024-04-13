9c07b46400181f2d4cf5af3c52d355481c05d8b0
Alan Hu <64699846+Alan-Hu123@users.noreply.github.com>
2024-04-13 14:26:31 -0400

    Added Images to show in ft page

--- client/app/foodtruck/[id]/page.js
+++ client/app/foodtruck/[id]/page.js
@@ -8,6 +8,7 @@ import Menu from "../../../compenents/Menu";
 import Address from "../../../compenents/address";
 import FTTitle from '../../../compenents/Title';
 import DropDown from "../../../compenents/dropDown";
+import ImageList from "@/compenents/imageList";
 import Link from "next/link";
 import { headers } from "next/headers";
 require("dotenv").config();
@@ -51,6 +52,27 @@ const fetchImage = async (id) => {
   }
 };
 
+const fetchImageList = async (id) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/foodtrucks/${id}/images`,
+      {
+        next: { revalidate: 10 }
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch food truck image");
+    }
+    const images = await response.json();
+
+    return images;
+    } catch (error) {
+      console.error("Error fetching food truck image:", error);
+    return null;
+  }
+};
+
 export async function generateMetadata({ params }) {
   const id = params.id;
   
@@ -73,6 +95,7 @@ export async function generateMetadata({ params }) {
 export default async function Page(slug) {
   const id = slug.params.id;
   const data = await fetchData(id);
+  const images = await fetchImageList(id);
 
   const headersList = headers();
   const URL = process.env.NEXT_PUBLIC_SITE_URL+ headersList.get('x-path');
@@ -93,9 +116,11 @@ export default async function Page(slug) {
         <Stack direction="column" flexGrow={1} gap={4} padding={4}>
             <FTTitle foodTruck={foodTruck}></FTTitle>
             <Address foodTruck ={foodTruck}></Address>
+            <ImageList images={images}></ImageList>
             <Menu foodTruck ={foodTruck}></Menu>
+            
             <Button variant="outlined" >
-              <Link href={"/"} style={{textDecoration: 'none', color: 'white'}} >Home</Link>
+              <Link href={'/'} style={{textDecoration: 'none', color: 'white'}} >Home</Link>
             </Button>
         </Stack>
 
---
+++ client/compenents/imageList.js
@@ -0,0 +1,24 @@
+import { Button, Card, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+
+export default function ImageList(props) {
+    const images = props.images;
+    return (
+        <>
+            <Card
+                sx={{
+                    backgroundColor: "#105372",
+                    padding: 3,
+                    height: "110px",
+                    overflowY: "auto",
+                }}
+            >
+                {
+                    images.map((item, index) => (
+                        <Image alt="test" src={item.imageUrl} key={index} height={100} width={100} />
+                    ))
+                }
+            </Card>
+        </>
+    )
+}
