255d652b37539c10101a19a97223b30007b8903d
Alan Hu <64699846+Alan-Hu123@users.noreply.github.com>
2024-05-15 04:07:27 -0400

    Revert "Merge branch 'main' into Use-Advanced-Markers"
    This reverts commit 19551536a5a7dff208067d2490ac64c4724d6ce4, reversing
    changes made to 2b51c263215495238f4e6869a1d67d2c81e3e424.

--- client/components/imageList.js
+++ client/components/imageList.js
@@ -42,27 +42,6 @@ const ImageWithPreviewModal = ({ url }) => {
 };
 
 export default function ImageList(props) {
-    const images = props.images;
-    return (
-        <>
-            <Card
-                sx={{
-                    backgroundColor: "#ffffff",
-                    padding: 3,
-                    height: "110px",
-                    overflowY: "auto",
-                }}
-            >
-                {
-                    images.map((item, index) => (
-                        <Image alt="test" src={item.imageUrl} key={index} height={100} width={100} />
-                    ))
-                }
-            </Card>
-        </>
-    )
-}
-=======
   const { id } = props;
   const [images, setImages] = useState([]);
 
